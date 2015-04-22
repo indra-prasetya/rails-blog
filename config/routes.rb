2a904fc3e19b145b077dc5aadd73c548016ba267
Indra Prasetya <punyaindra@gmail.com>
2015-04-22 16:10:26 +0700

    added devise for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   resources :posts
+  
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
