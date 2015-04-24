e91e861a6475f7657397ddb61163f4eec25449e2
Indra Prasetya <punyaindra@gmail.com>
2015-04-24 10:00:17 +0700

    add tag and category controller

---
+++ app/controllers/category_controller.rb
@@ -0,0 +1,74 @@
+class CategoryController < ApplicationController
+  before_filter :authenticate_user!
+  
+  # GET /categories
+  # GET /categories.json
+  def index
+    @categorys = Category.all
+  end
+
+  # GET /categories/1
+  # GET /categories/1.json
+  def show
+  end
+
+  # GET /categories/new
+  def new
+    @category = Category.new
+  end
+
+  # GET /categories/1/edit
+  def edit
+  end
+
+  # Category /categories
+  # Category /categories.json
+  def create
+    @category = Category.new(tag_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /categories/1
+  # PATCH/PUT /categories/1.json
+  def update
+    respond_to do |format|
+      if @category.update(Category_params)
+        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.json { render :show, status: :ok, location: @category }
+      else
+        format.html { render :edit }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /categories/1
+  # DELETE /categories/1.json
+  def destroy
+    @category.destroy
+    respond_to do |format|
+      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tag
+      @category = Category.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def tag_params
+      params.require(:category).permit(:name, :active)
+    end
+end
