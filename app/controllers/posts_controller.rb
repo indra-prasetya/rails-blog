eae2ae819a7e27c6044fe8420ee80a33ecf3c325
Indra Prasetya <punyaindra@gmail.com>
2015-04-28 14:08:52 +0700

    add admin checking

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, :except => [:show, :index]
-
+  
   # GET /posts
   # GET /posts.json
   def index
